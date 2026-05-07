91eafffe0d7680797ec3bccd51f2da56b009ce52
kennysantanu <kennysantanu@users.noreply.github.com>
2026-05-07 08:45:02 +0000

    deploy: af3ba33b529b02a601a9fad8369d957805620de9

---
+++ _app/immutable/nodes/0.CR3Y0NdS.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/BTUkrOiQ.js";import{g as p}from"../chunks/CYTyRVBJ.js";import{s}from"../chunks/BgHlbA-D.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
